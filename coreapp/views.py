5db1257333a77381accc2f68abeeacc7e7fa6efd
maqsood <maqsoodhamd37@gmail.com>
2021-02-25 22:03:26 +0530

    [Maqsood]: connected home page to the backend

--- coreapp/views.py
+++ coreapp/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-
+from .models import Dish
 # Create your views here.
 def home(request):
-    return render(request,'index.html')
+    menu=dict()
+    menu['Starters']=Dish.objects.filter(dish_type='Starters')|Dish.objects.filter(dish_type='Momos')|Dish.objects.filter(dish_type='Soups')
+    menu['Main_Dishes']=Dish.objects.filter(dish_type='Main Course')|Dish.objects.filter(dish_type='Bread')|Dish.objects.filter(dish_type='Others')
+    menu['Noodles_and_Rice']=Dish.objects.filter(dish_type='Fried Rice and Noodles')
+    menu['Rolls']=Dish.objects.filter(dish_type='Rolls')
+    return render(request,'index.html',{'menu':menu})
 
 def reservation(request):
     return render(request,'reservation.html')
